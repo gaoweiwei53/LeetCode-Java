0ef65d86eaa1cc763cc30f421126318de205c17c
alex <782324077@qq.com>
2020-09-30 16:51:18 +0800

    9/30

---
+++ src/BackTracking/p78_subsets/Solution.java
@@ -0,0 +1,46 @@
+package p78_subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        List<List<Integer>> result = new Solution().subsets(nums);
+        System.out.println(result);
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        backtrack(list, new ArrayList<Integer>(), nums, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
+        //走过的所有路径都是子集的一部分，所以都要加入到集合中
+        list.add(new ArrayList<>(tempList));
+        for (int i = start; i < nums.length; i++) {
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+    /*
+    public static List<List<Integer>> subsets(int[] nums) {
+        //子集的长度是2的nums.length次方，这里通过移位计算
+        int length = 1 << nums.length;
+        List<List<Integer>> res = new ArrayList<>(length);
+        //遍历从0到length中间的所有数字，根据数字中1的位置来找子集
+        for (int i = 0; i < length; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < nums.length; j++) {
+                //如果数字i的某一个位置是1，就把数组中对
+                //应的数字添加到集合
+                if (((i >> j) & 1) == 1)
+                    list.add(nums[j]);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+    */
+}
