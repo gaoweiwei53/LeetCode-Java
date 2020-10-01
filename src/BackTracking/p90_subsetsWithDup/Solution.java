d604b875019a91e3e6dde9721495c11f2bf40557
alex <782324077@qq.com>
2020-10-01 09:59:16 +0800

    10/01

---
+++ src/BackTracking/p90_subsetsWithDup/Solution.java
@@ -0,0 +1,25 @@
+package p90_subsetsWithDup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        backtrack(list, new ArrayList<Integer>(), nums, 0);
+        return list;
+    }
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
+        //走过的所有路径都是子集的一部分，所以都要加入到集合中
+        list.add(new ArrayList<>(tempList));
+        for (int i = start; i < nums.length; i++) {
+            // 其他的代码和 78题没区别
+            if (i > start && nums[i] == nums[i-1]) continue;
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
