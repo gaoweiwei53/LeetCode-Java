36a6219ac2911fbb7fff17c0e3d9c29e313695f5
alex <782324077@qq.com>
2021-04-17 15:19:36 +0800

    130

--- src/BinaryTree/p129_Sum_Root_to_Leaf_Numbers/Solution.java
+++ src/BinaryTree/p129_Sum_Root_to_Leaf_Numbers/Solution.java
@@ -1,6 +1,10 @@
 package p129_Sum_Root_to_Leaf_Numbers;
 
-  class TreeNode {
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
       int val;
       TreeNode left;
       TreeNode right;
@@ -13,7 +17,59 @@ package p129_Sum_Root_to_Leaf_Numbers;
       }
   }
 class Solution {
+    // 深度优先遍历
+/*    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+    public int dfs(TreeNode root, int prevSum) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = prevSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        } else {
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
+
+
+    }*/
+    // 广度优先遍历
+
+    // 需要维护两个队列，分别存储节点和 节点到根节点对应的数字。
     public int sumNumbers(TreeNode root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        Queue<Integer> numQueue = new LinkedList<>();
+        nodeQueue.offer(root);
+        numQueue.offer(root.val);
+
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.poll();
+            int num = numQueue.poll();
+            TreeNode left = node.left, right = node.right;
+
+            // 如果是叶子节点，则将该叶子节点到根节点形成的数加到sum中
+            if (left == null && right == null) {
+                sum += num;
+            } else {
+                if (left != null) {
+                    nodeQueue.offer(left);
+                    // 节点到根节点对应的数字
+                    numQueue.offer(num * 10 + left.val);
+                }
+                if (right != null) {
+                    nodeQueue.offer(right);
+                    numQueue.offer(num * 10 + right.val);
+                }
+            }
+        }
+        return sum;
     }
 }
+
+// 每个节点都对应一个数字，等于其父节点对应的数字乘以 10 再加上该节点的值（这里假设根节点的父节点对应的数字是 0）
---
+++ src/UnionFind/unionfind/UnionFind.java
@@ -0,0 +1,77 @@
+package unionfind;
+
+import java.util.Arrays;
+
+// 开启了路径压缩和按秩合并的并查集
+public class UnionFind {
+    int[] parent;
+
+    //  i的根节点的子集的大小 亦称为秩
+    int[] size;
+    // 子集的个数 亦称连通分量
+    int branchCount;
+
+    public UnionFind(int n) {
+        this.branchCount = n;
+        this.parent = new int[n];
+        this.size = new int[n];
+        Arrays.fill(size, 1);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    // 查找根节点
+    public int find(int x) {
+        // 路径压缩
+        return parent[x] == x ? x : (parent[x] = find(parent[x]));
+    }
+
+    public boolean union (int x, int y) {
+         int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY) {
+            return false;
+        }
+        // 按秩合并, 选取较大的子集的根节点作为合并的根节点
+        if (size[rootX] <= size[rootY]){
+            parent[rootX] = parent[rootY];
+            size[rootY] +=  size[rootX];
+        }
+        else{
+            parent[rootY] = parent[rootX];
+            size[rootX] +=  size[rootY];
+        }
+        // 减少了一棵树，则连通分量 -1
+        --branchCount;
+        return true;
+    }
+    public boolean isConnected(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    public int branchCount() {
+        return branchCount;
+    }
+}
+class Test{
+    public static void main(String[] args) {
+        UnionFind unionFind = new UnionFind(7);
+        int[][] edges = {{0, 1}, {1,2},{1,3},{2,4},{3,4},{6,5}};
+        for (int i = 0; i < 6; i++) {
+            int x = edges[i][0];
+            int y = edges[i][1];
+            unionFind.union(x, y);
+        }
+        for (int i = 0; i < 7; i++) {
+            System.out.print(unionFind.parent[i]+ " ");
+        }
+        System.out.println();
+        for (int i = 0; i < 7; i++) {
+            System.out.print(unionFind.size[i]+ " ");
+        }
+        System.out.println();
+        System.out.println(unionFind.branchCount);
+    }
+}
