9641ea313e0572d2c2ecc73b85a2036ccf085729
alex <782324077@qq.com>
2021-05-24 14:43:12 +0800

    5/24

---
+++ src/Array/p1_Two_Sum/Solution.java
@@ -0,0 +1,32 @@
+package p1_Two_Sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            int remain = target - nums[i];
+            for(int j = i + 1; j < n; j++){
+                if(remain == nums[j]){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    // 暴力解法有点慢
+    // 使用hash表
+    public int[] twoSum2(int[] nums, int target) {
+        int n = nums.length;
+        Map<Integer,Integer> map = new HashMap();
+        for (int i = 0; i < n; i++){
+            if (map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]),i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{0};
+    }
+}
--- src/DynamicPrograming/p5_longestpalindromicsubstring/Solution.java
+++ src/DynamicPrograming/p5_longestpalindromicsubstring/Solution.java
@@ -48,6 +48,8 @@ public class Solution {
         int begin = 0;
 
         // dp[i][j] 表示 s[i...j] 是否是回文串
+        // 状态转移方程: dp[i][j] = dp[i-1][j+1] && (s[i] == s[j])
+        // 边界条件：j - 1 - (i+1) + 1 < 2, 整理的 j - i < 3, 状态转移方程建立在子串长度大于2的前提下
         // 默认为false
         boolean[][] dp = new boolean[len][len];
         for (int i = 0; i < len; i++) {
@@ -75,6 +77,101 @@ public class Solution {
         }
         return s.substring(begin, begin + maxlen);
 
+    }
+    // 中心扩散法
+    public String longestPalindrome1(String s) {
+
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        int strLen = s.length();
+        int left = 0;
+        int right = 0;
+        int len = 1;
+        int maxStart = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < strLen; i++) {
+            left = i - 1;
+            right = i + 1;
+            // 向左寻找与当前相等的字符
+            while (left >= 0 && s.charAt(left) == s.charAt(i)) {
+                len++;
+                left--;
+            }
+            // 向右寻找与当前相等的字符
+            while (right < strLen && s.charAt(right) == s.charAt(i)) {
+                len++;
+                right++;
+            }
+            // 向中心的两边寻找对称相等的字符
+            while (left >= 0 && right < strLen && s.charAt(right) == s.charAt(left)) {
+                len = len + 2;
+                left--;
+                right++;
+            }
+            // 记录最长回文字符串的起始位置
+            if (len > maxLen) {
+                maxLen = len;
+                maxStart = left;
+            }
+            len = 1;
+        }
+        return s.substring(maxStart + 1, maxStart + maxLen + 1);
+    }
+    // 分为两种情况：
+    // 1. 中心字符与相邻两边不相等
+    // 2. 中心字符与相邻两边相等，此时要把所有相等的字符串作为中心
+
+
+    // 动态规划
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        int maxLen = 1;
+        int begin = 0;
+        // dp[i][j] 表示 s[i..j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        // 初始化：所有长度为 1 的子串都是回文串
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        char[] charArray = s.toCharArray();
+        // 递推开始
+        // 先枚举子串长度
+        for (int L = 2; L <= len; L++) {
+            // 枚举左边界，左边界的上限设置可以宽松一些
+            for (int i = 0; i < len; i++) {
+                // 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+
+                // 右边界
+                int j = L + i - 1;
+                // 如果右边界越界，就可以退出当前循环
+                if (j >= len) {
+                    break;
+                }
+
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
     }
 
 }
