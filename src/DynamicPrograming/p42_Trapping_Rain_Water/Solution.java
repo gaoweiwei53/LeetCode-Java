ec35cab52c8a10fd0ae98af75d9de9de9aa15f28
alex <782324077@qq.com>
2021-05-26 11:38:20 +0800

    5/26

---
+++ src/DynamicPrograming/p42_Trapping_Rain_Water/Solution.java
@@ -0,0 +1,76 @@
+package p42_Trapping_Rain_Water;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution {
+    // 动态规划
+    // ！某点的可盛水的量等于左右最小高度减去该点的高度
+    // 思路：使用两个辅助数组分别记录左右较大高度
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; ++i) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
+    }
+    // 单调栈
+    // 维护一个单调栈，单调栈存储的是下标，满足从栈底到栈顶的下标对应的数组中的元素递减
+    public int trap2(int[] height) {
+        int ans = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        int n = height.length;
+        for (int i = 0; i < n; ++i) {
+            // 当当前高度大于栈顶元素时，可盛水。取出top
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                int top = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                int left = stack.peek();
+                int currWidth = i - left - 1;
+                int currHeight = Math.min(height[left], height[i]) - height[top];
+                ans += currWidth * currHeight;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+
+    //双指针
+    // 使用两个指针分别指向数组两侧，并用height[left]和height[right]分别更新leftMax和rightMax
+    public int trap3(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                ++left;
+            } else {
+                ans += rightMax - height[right];
+                --right;
+            }
+        }
+        return ans;
+    }
+}
--- src/DynamicPrograming/p70_Climbing_Stairs/Solution.java
+++ src/DynamicPrograming/p70_Climbing_Stairs/Solution.java
@@ -10,4 +10,16 @@ class Solution {
         }
         return dp[n];
     }
+    public int climbStairs2(int n) {
+        if(n <= 2) return n;
+        else{
+            int[] dp = new int[n];
+            dp[0] = 1;
+            dp[1] = 2;
+            for(int i = 2; i < n; i++){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            return dp[n-1];
+        }
+    }
 }
