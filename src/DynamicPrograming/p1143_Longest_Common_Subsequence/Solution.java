d29fce63ad022d735ec2ba8338f415e4b8a891a0
alex <782324077@qq.com>
2021-05-30 18:10:42 +0800

    2/30

---
+++ src/DynamicPrograming/p1143_Longest_Common_Subsequence/Solution.java
@@ -0,0 +1,34 @@
+package p1143_Longest_Common_Subsequence;
+
+class Solution {
+
+    /**
+     * 最长公共子序列问题是典型的二维动态规划问题
+     * dp[i][j]表示的是s1[0:i-1]和 s2[0:j-1]的最长公共子序列的长度，不需要连续
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            // charAt(i - 1)
+            char c1 = text1.charAt(i - 1);
+            for (int j = 1; j <= n; j++) {
+                // charAt(i - 1)
+                char c2 = text2.charAt(j - 1);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    public static void main(String[] args) {
+        String s = "qwer";
+        System.out.println(s.charAt(0));
+
+    }
+}
+
---
+++ src/DynamicPrograming/p198_House_Robber/Solution.java
@@ -0,0 +1,45 @@
+package p198_House_Robber;
+
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        // dp[i] 直至当前的房子所能偷窃的最大金额
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            // 第i个房子偷窃或不偷窃，有两种状态
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1] );
+        }
+        return dp[length - 1];
+    }
+
+    /**
+     * 考虑到每间房屋的最高总金额只和该房屋的前两间房屋的最高总金额相关，因此可以使用滚动数组，
+     * 在每个时刻只需要存储前两间房屋的最高总金额。
+     */
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int first = nums[0], second = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+}
+
